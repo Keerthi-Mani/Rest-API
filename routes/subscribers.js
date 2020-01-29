a1749e814b8ebbe33dcfe8b3c6b436ddb3ca144e
Keerthi Mani <carkeane.getz@gmail.com>
2020-01-29 09:36:30 -0500

    routes are provided

---
+++ routes/subscribers.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express.Router();
+
+//Getting all the subscribers
+router.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+//Getting One subscriber
+router.get("/:id", (req, res) => {
+  req.params.id;
+});
+
+//Creating One
+router.post("/", (req, res) => {
+  req.params.id;
+});
+
+//Updating One
+router.patch("/:id", (req, res) => {
+  req.params.id;
+});
+
+//Deleting One
+router.get("/:id", (req, res) => {
+  req.params.id;
+});
+
+module.exports = router;
